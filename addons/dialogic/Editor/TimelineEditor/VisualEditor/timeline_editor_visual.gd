ddeb214b232234a402fd80ae4029458e1481d1e2
notme84 <nguster@gmail.com>
2025-01-02 22:40:04 -0800

    fixed Dialogic2 import plugin

--- addons/dialogic/Editor/TimelineEditor/VisualEditor/timeline_editor_visual.gd
+++ addons/dialogic/Editor/TimelineEditor/VisualEditor/timeline_editor_visual.gd
@@ -312,11 +312,6 @@ func _on_event_block_gui_input(event: InputEvent, item: Node) -> void:
 
 			drag_allowed = true
 
-		if event.is_released() and not %TimelineArea.dragging and not Input.is_key_pressed(KEY_SHIFT):
-			if len(selected_items) > 1 and item in selected_items and not Input.is_key_pressed(KEY_CTRL):
-				deselect_all_items()
-				select_item(item)
-
 	if len(selected_items) > 0 and event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			if !%TimelineArea.dragging and !get_viewport().gui_is_dragging() and drag_allowed:
@@ -430,12 +425,12 @@ func get_events_indexed(events:Array) -> Dictionary:
 		if event.resource is DialogicEndBranchEvent:
 			continue
 
-		indexed_dict[event.get_index()] = event.resource._store_as_string()
+		indexed_dict[event.get_index()] = event.resource.to_text()
 
 		# store an end branch if it is selected or connected to a selected event
 		if 'end_node' in event and event.end_node:
 			event = event.end_node
-			indexed_dict[event.get_index()] = event.resource._store_as_string()
+			indexed_dict[event.get_index()] = event.resource.to_text()
 		elif event.resource is DialogicEndBranchEvent:
 			if event.parent_node in events: # add local index
 				indexed_dict[event.get_index()] += str(events.find(event.parent_node))
@@ -476,7 +471,7 @@ func add_events_indexed(indexed_events:Dictionary) -> void:
 				event_resource = i.duplicate()
 				break
 
-		event_resource._load_from_string(indexed_events[event_idx])
+		event_resource.from_text(indexed_events[event_idx])
 
 		# now create the visual block.
 		deselect_all_items()
@@ -547,7 +542,7 @@ func copy_selected_events() -> void:
 
 	var event_copy_array := []
 	for item in selected_items:
-		event_copy_array.append(item.resource._store_as_string())
+		event_copy_array.append(item.resource.to_text())
 		if item.resource is DialogicEndBranchEvent:
 			if item.parent_node in selected_items: # add local index
 				event_copy_array[-1] += str(selected_items.find(item.parent_node))
@@ -1104,17 +1099,16 @@ func _input(event:InputEvent) -> void:
 			get_viewport().set_input_as_handled()
 
 		"Ctrl+C":
-			select_events_indexed(get_events_indexed(selected_items))
 			copy_selected_events()
 			get_viewport().set_input_as_handled()
 
 		"Ctrl+V":
 			var events_list := get_clipboard_data()
-			var paste_position := 0
+			var paste_position := -1
 			if selected_items:
 				paste_position = selected_items[-1].get_index()+1
 			else:
-				paste_position = %Timeline.get_child_count()
+				paste_position = %Timeline.get_child_count()-1
 			if events_list:
 				TimelineUndoRedo.create_action("[D] Pasting "+str(len(events_list))+" event(s).")
 				TimelineUndoRedo.add_do_method(add_events_at_index.bind(events_list, paste_position))
--- addons/dialogic/Resources/dialogic_style.gd
+++ addons/dialogic/Resources/dialogic_style.gd
@@ -132,6 +132,7 @@ func move_layer(from_index:int, to_index:int) -> void:
 func set_layer_scene(layer_id:String, scene:String) -> void:
 	if not has_layer(layer_id):
 		return
+
 	layer_info[layer_id].scene = load(scene)
 	changed.emit()
 
@@ -246,8 +247,8 @@ func clone() -> DialogicStyle:
 	style.inherits = inherits
 
 	var base_info := get_layer_info("")
-	style.set_layer_scene("", base_info.path)
-	style.set_layer_overrides("", base_info.overrides)
+	set_layer_scene("", base_info.path)
+	set_layer_overrides("", base_info.overrides)
 
 	for id in layer_list:
 		var info := get_layer_info(id)
