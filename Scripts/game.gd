fee61f51474a31906f9578eb9dc7845a90c8db36
notme84 <nguster@gmail.com>
2025-01-05 11:01:51 -0800

    basic dialogic variable test

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -5,13 +5,23 @@ extends Node2D
 func _ready():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 	Dialogic.start(Timeline.resource_path)	
+	print("Toggle is ", Dialogic.VAR.Toggle)
+	print("Count is ", Dialogic.VAR.Count)
+	
 
 func _on_dialogic_signal(argument: String):
+	#REMINDER dialogic2 finish signal logic before continuing timeline
 	if argument == "introEnded":
-		print("Toggle is ", Dialogic.VAR.Toggle)
-		print("Count is ", Dialogic.VAR.Count)
-		Dialogic.VAR.Count += 3
-		print("Count is ", Dialogic.VAR.Count)
 		print(argument)
+		print("Count is ", Dialogic.VAR.Count)
+		Dialogic.VAR.Count += 1
+		print("Count is ", Dialogic.VAR.Count)
+	elif argument == "choiceCount":
+		print(argument)
+		print("Count is ", Dialogic.VAR.Count)
 	elif argument == "mainMenu":
+		print(argument)
 		get_tree().change_scene_to_file("res://Scenes/Menu/MainMenu.tscn")
+	else:
+		print(argument)
+		print("Count is ", Dialogic.VAR.Count)
