4ff5143adc6d75097fa396433f4dafc134d1a015
notme84 <nguster@gmail.com>
2025-01-05 02:02:52 -0800

    basic variable setup

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -8,6 +8,10 @@ func _ready():
 
 func _on_dialogic_signal(argument: String):
 	if argument == "introEnded":
+		print("Toggle is ", Dialogic.VAR.Toggle)
+		print("Count is ", Dialogic.VAR.Count)
+		Dialogic.VAR.Count += 3
+		print("Count is ", Dialogic.VAR.Count)
 		print(argument)
 	elif argument == "mainMenu":
 		get_tree().change_scene_to_file("res://Scenes/Menu/MainMenu.tscn")
